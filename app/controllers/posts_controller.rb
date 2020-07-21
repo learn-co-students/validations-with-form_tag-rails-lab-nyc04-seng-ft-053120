7e8089630ce345c8ef9d8b32a4fffc7d8af6f46e
tracycollins <tc@threeceemedia.com>
2020-07-21 16:22:33 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,28 @@
 class PostsController < ApplicationController
+  
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @author = Author.new
+  end
+
 
   def edit
     @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
 
+    @post = Post.find(params[:id])
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
